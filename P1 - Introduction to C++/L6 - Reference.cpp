5a098320bf20f4a2cfe28770d6fd4ba139c33fa3
Chirag Goyal <99237546+cgagarwal@users.noreply.github.com>
2022-02-15 19:35:27 +0530

    Create L6 - Reference.cpp

---
+++ P1 - Introduction to C++/L6 - Reference.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int c = 72;     //  global variable
+
+int main() {
+    int a = 8, b = 5;
+    int c = a + b;      // local variable
+
+    cout << c << endl;      //  13 (local)
+    cout << ::c << endl;    // 72 (global)
+    // :: --> scope resolution operator
+    // use for accessing global variable c
+
+    float d = 34.4f;        // f --> float (F also used)
+    long double e = 34.4l;  // l -> long double
+    // 34.4 is a double
+
+  
+    // Reference Variables
+    float x = 45;
+    float &y = x;   // y is reference to x
+    x++;
+    cout << y << endl;  // 46
+    // y is not another var , it is aliases to x
+    // if we update x , y also changed
+
+
+    // Typecasting
+    float k = 4.56;
+    cout << k << endl;      // 4.56
+    cout << (int)k << endl;         // 4
+    cout << int(k) << endl;         // 4
+    
+    return 0;
+}
