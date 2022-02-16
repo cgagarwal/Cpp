b9a061cd91bab937f7f50851066a2b9f56bfca73
Chirag Goyal <99237546+cgagarwal@users.noreply.github.com>
2022-02-16 14:21:23 +0530

    Create L6 - Function Overloading.cpp

---
+++ P2 - Functions in C++/L6 - Function Overloading.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int add(int x , int y) {
+    return x+y + 1;
+}   //  add1
+
+float add(float x , float y) {
+    return x+y+ 1.4;
+}   // add2
+
+
+int vol(int l , int b , int h) {
+    cout << "Cuboidal\t";
+    return l*b*h;
+}
+
+int vol(int a) {
+    cout << "Cube\t";
+    return a*a*a;
+}
+
+int main() {
+  
+    // overwriting functions feature of C++
+
+    cout << add(2,4) << endl;       // add1 used
+    cout << add(2.1f,4.3f) << endl; // add2 used
+
+    cout << vol(2,4,6) << endl;     // Cuboidal     48
+    cout << vol(3) << endl;         // Cube         27
+
+    return 0;
+}
