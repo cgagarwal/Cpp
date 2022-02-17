49761ed0b435771df3b0adb00f190d0d2c15e87f
Chirag Goyal <99237546+cgagarwal@users.noreply.github.com>
2022-02-17 23:24:55 +0530

    Create P11 - String Algorithms.cpp

---
+++ P2 - Functions in C++/P11 - String Algorithms.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    // sort the string
+    string str = "vaikunthapurramuloo";
+    sort(str.begin(), str.end());       // sort(itr to start, itr to end)
+    cout << str << endl;        // aaahiklmnooprrtuuuv
+
+    // sort in descending order
+    sort(str.begin(), str.end() , greater<int>());
+    cout << str << endl;        // vuuutrrpoonmlkihaaa
+
+  
+    // transform the string (in uppercase and in lowercase)
+    // transform(itr to start, itr to last, itr to where result is stored, unary_operator)
+    string s = "KhiLadi";
+    transform(s.begin() , s.end() , s.begin() , ::tolower);
+    cout << s << endl;      // khiladi
+
+    s = "Duvvada Jagannadham";
+    transform(s.begin() , s.end() , s.begin() , ::toupper);
+    cout << s << endl;      // DUVVADA JAGANNADHAM
+
+    return 0;
+}
