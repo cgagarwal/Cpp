88234cdb677cf8b24b6a15023f5c2a33106840b4
Chirag Goyal <99237546+cgagarwal@users.noreply.github.com>
2022-03-12 12:47:49 +0530

    Create L5 - Destructors.cpp

---
+++ P3 - Classes in C++/L5 - Destructors.cpp
@@ -0,0 +1,43 @@
+/* ------------------------------ Destructors in C++ -----------------------------------
+---- special member function with same name as of class with a tilde(~).
+---- neither takes any argument nor does it return any value.
+---- it invoked when any object is destroyed and there should be only one destructor in a class.
+*/
+
+#include <iostream>
+using namespace std;
+
+class num{
+    int ct;
+    
+    public:
+        num(int n) {                     // Constructor
+            ct = n;
+            cout << "Constructor invoked for " << ct << endl;
+        }
+
+        ~num() {                     // Destructor
+            cout << "Destructor invoked for " << ct << endl;
+        }
+};
+
+int main() {
+    
+    num n1(1);             // Constructor for n1
+    num n5(5);
+  
+    {   
+        // scope of object declared here is only inside this block
+        num n2(2), n3(3);         // Constructor for n2 , n3
+        num n4(4);
+        num n6(6);
+        
+    }
+    // destructor called in reverse order (n6 , n4 , n3 , n2)
+    
+    num n7(7);         // Constructor for n7
+
+    // destructor ----> at the end of program   (n7 , n5 , n1)
+
+    return 0;
+}
