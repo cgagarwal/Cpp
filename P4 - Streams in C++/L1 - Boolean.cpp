18323b55cdce694ce70667b8c889f506c3b4e7d2
Chirag Goyal <99237546+cgagarwal@users.noreply.github.com>
2022-03-20 13:05:26 +0530

    Update L1 - Boolean.cpp

--- P4 - Streams in C++/L1 - Boolean.cpp
+++ P4 - Streams in C++/L1 - Boolean.cpp
@@ -3,31 +3,32 @@
 using namespace std;
 
 int main() {
+
+   // Stream Manipulators  ----- Boolean
+
+   bool a = true, b = false;
+
+   // Default boolean values 1 and 0 (i.e noboolalpha)
+
+   cout << a << '\t' << b << endl;      //  1    0
+
+   cout << boolalpha;
+   cout << a << '\t' << b << endl;      // true   false
+   cout << noboolalpha;
+   cout << a << '\t' << b << endl;      //  1    0
+
+   // Using setf method  --> Toogle to true/false
+
+   cout.setf(ios::boolalpha);
+   cout << a << '\t' << b << endl;      // true   false
+
+   // Toogle to default 0/1 
+
+   cout << resetiosflags(ios::boolalpha);
+   cout << a << '\t' << b << endl;      //  1    0
+
+   // All the settings reamain same unless flags are again changed
+
+   return 0;
   
-  // Stream Manipulators  ----- Boolean
-  
-  bool a = true, b = false;
-
-  // Default boolean values 1 and 0 (i.e noboolalpha)
-
-  cout << a << '\t' << b << endl;      //  1    0
-
-  cout << boolalpha;
-  cout << a << '\t' << b << endl;      // true   false
-  cout << noboolalpha;
-  cout << a << '\t' << b << endl;      //  1    0
-
-  // Using setf method  --> Toogle to true/false
-
-  cout.setf(ios::boolalpha);
-  cout << a << '\t' << b << endl;      // true   false
-  
-  // Toogle to default 0/1 
-
-  cout << resetiosflags(ios::boolalpha);
-  cout << a << '\t' << b << endl;      //  1    0
-
-  // All the settings reamain same unless flags are again changed
-
-  return 0;
 }
