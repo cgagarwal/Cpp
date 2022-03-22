47092b8ad17359f204020f17dae564df02cf1597
Chirag Goyal <99237546+cgagarwal@users.noreply.github.com>
2022-03-22 23:15:49 +0530

    Create L9 - Maps STL.cpp

---
+++ P5 - Templates in C++/L9 - Maps STL.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <map>          // key : value pairs
+#include <unordered_map>    //  no duplicates allowed   ;  elements unordered
+using namespace std;
+
+void display(map<string,int> &movie) {
+    map<string,int> :: iterator mp = movie.begin();     // iterate over keys
+    while (mp != movie.end()) {
+        cout << (*mp).first << " : " << (*mp).second << endl;
+        mp++;
+    }
+}
+
+int main() {
+
+    map<string, int> movie;
+    movie["Allu Arjun"] = 22;
+    movie["Ram Charan Teja"] = 15;
+    movie["Ravi Teja"] = 56;
+    movie["Ram Pothineni"] = 19;
+
+    movie.at("Allu Arjun") = 24;      // only for updation
+    
+    movie.insert(make_pair("Vijay", 48));   // inserting in map using insert
+
+    cout << movie.size() << endl;   // 4
+
+    display(movie);
+    cout << endl;
+
+    cout << movie.empty() << endl;  // 0
+
+  
+    movie["Vijay"] = 52;    // both update and initialise
+    
+    movie.erase("Ram Charan Teja");     // remove by key value
+
+
+    map<string,int> :: iterator mp = movie.find("Vijay");
+
+    if (mp != movie.end())
+        cout << (*mp).first << " : " << (*mp).second << endl;
+
+    movie.clear();
+    cout << movie.empty() << endl;  // 1
+
+    // multimap and unordered_multimap            ; allow duplicate element
+
+    return 0;
+}
