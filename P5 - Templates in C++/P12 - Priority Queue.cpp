6c8e42df074f0fff1deba92923d465bcbe615302
Chirag Goyal <99237546+cgagarwal@users.noreply.github.com>
2022-03-23 10:14:39 +0530

    Create P12 - Priority Queue.cpp

---
+++ P5 - Templates in C++/P12 - Priority Queue.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <queue>        // priority queue
+using namespace std;
+
+int main() {
+
+    // insertion and removal from front
+    // stroed internally as vector by default
+    // element inserted in priority order (largest value has largest priority at front)
+    // for inserting object of any class in priority queue it must has overloaded operator '<'
+    // No iterators supported
+
+    priority_queue<int> pq;
+
+    pq.push(10);
+    pq.push(20);        // inserted in priority order
+    pq.push(5);
+    pq.push(8);
+
+    cout << pq.top() << endl;       // 20
+
+    pq.pop();       // highest priority removed (20)
+
+    cout << pq.size() << endl;      // 3
+
+    while (!pq.empty()) {
+        cout << pq.top() << " ";
+        pq.pop();
+    }
+    cout << endl;       // 10 8 5
+
+    return 0;
+}
