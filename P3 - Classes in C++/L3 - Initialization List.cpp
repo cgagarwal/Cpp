872c59f3ed4cfe715b053d4fdfe0a0daaf0ea6a9
Chirag Goyal <99237546+cgagarwal@users.noreply.github.com>
2022-03-27 12:20:35 +0530

    Update L3 - Initialization List.cpp

--- P3 - Classes in C++/L3 - Initialization List.cpp
+++ P3 - Classes in C++/L3 - Initialization List.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Initialization list helps to declare the value of members in initialization section
+// Initialization list helps to declare the value of members when they are initialising
 
 class Fun {
     int x,y;
@@ -14,11 +14,15 @@ class Fun {
         // Fun(int i, int j) : x(i), y(i+j*2)  ---->   x = i, y = i+j*2
         // Fun(int i, int j) : y(x+i) , x(j)   ---->   x = j , y = x+i
         // Fun(int i, int j) : y(i) , x(y+j)   ---->   x = garbage_value_of_y + j , y = i
+    
+        // Fun(int x, int y) : x(x) , y(y)     ---->   No need of this pointer
+    
+        // Fun(int x, int y) : x(2*x) , y(y+x)  ; (x & y inside brackets are arguments)     ---> VALID
 
         Fun(int i, int j) : x(i), y(x+j) {
             cout << "Constructor invoked." << endl;
-            cout << "Value of x is " << x << endl;
-            cout << "Value of y is " << y << endl;
+            cout << "Value of x is " << x << endl;          // i
+            cout << "Value of y is " << y << endl;          // x+j
         }
 
 };
